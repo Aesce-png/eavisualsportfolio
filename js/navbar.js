8c6790ed00b9e102dd3e9fa883b93d4e12735491
Eugene Ace Pascual <eugeneacepascual@gmail.com>
2026-05-17 03:51:00 +0800

    Add files via upload

---
+++ js/navbar.js
@@ -0,0 +1,6 @@
+fetch('../htmls/navbar.html')
+  .then(response => response.text())
+  .then(html => {
+    document.body.insertAdjacentHTML('afterbegin', html);
+  })
+  .catch(err => console.error('Error loading navbar:', err));
